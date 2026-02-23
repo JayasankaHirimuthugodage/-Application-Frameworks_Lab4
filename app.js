86f5a1d066ebec29a489b5a87a0f0612e4499737
Jayasanka Hirimuthugodage <hirimuthugodagejayasanka@gmail.com>
2026-02-23 12:34:24 +0530

    Implemented read file feature

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log("Lab 04 -Initial Setup Successful");
+
+
+const fs = require('fs'); 
+fs.readFile('file.txt', 'utf8', function (err, data) { 
+if (err) throw err; 
+console.log(data); 
+}); 
